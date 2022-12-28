1c0a378475702aedd2f4d04d0e8599435290239b
Marcio Moura <marcioavb.ms@gmail.com>
2022-12-28 09:34:03 -0300

    inicia o projeto

---
+++ src/main/java/br/com/showbike/clientebike/ClienteBikeApplication.java
@@ -0,0 +1,13 @@
+package br.com.showbike.clientebike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteBikeApplication.class, args);
+	}
+
+}
