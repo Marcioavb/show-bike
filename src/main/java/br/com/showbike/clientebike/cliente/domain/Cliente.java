7094e408de5230f6826b8808c7d76928f02d73ca
Marcio Moura <marcioavb.ms@gmail.com>
2022-12-28 15:40:43 -0300

    implementa dominio de negocio

---
+++ src/main/java/br/com/showbike/clientebike/cliente/domain/Cliente.java
@@ -0,0 +1,56 @@
+package br.com.showbike.clientebike.cliente.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import org.hibernate.validator.constraints.br.CPF;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(columnDefinition = "uuid", name = "id", updatable = false, unique = true, nullable = false)
+	private UUID idCliente;
+	@NotBlank
+	private String nomeCompleto;
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	private String celular;
+	private String telefone;
+	private Sexo sexo;
+	@NotNull
+	private LocalDate dataNascimento;
+	@CPF
+	private String cpf;
+
+	private LocalDateTime datahoraDoCadastro;
+	private LocalDateTime dataHoraDaUltimaAlteracao;
+
+	public Cliente(UUID idCliente, @NotBlank String nomeCompleto, @NotBlank @Email String email,
+			@NotBlank String celular, String telefone, Sexo sexo, @NotNull LocalDate dataNascimento, @CPF String cpf) {
+		this.nomeCompleto = nomeCompleto;
+		this.email = email;
+		this.celular = celular;
+		this.telefone = telefone;
+		this.sexo = sexo;
+		this.dataNascimento = dataNascimento;
+		this.cpf = cpf;
+		this.datahoraDoCadastro = LocalDateTime.now();
+	}
+}
