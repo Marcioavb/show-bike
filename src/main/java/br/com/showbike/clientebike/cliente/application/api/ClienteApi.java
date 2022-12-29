5b05bc815148abfb8e4bc370abc0137ad343ac33
Marcio Moura <marcioavb.ms@gmail.com>
2022-12-29 10:22:27 -0300

    modifica cliete para cliente e

--- src/main/java/br/com/showbike/clientebike/cliente/application/api/ClienteApi.java
+++ src/main/java/br/com/showbike/clientebike/cliente/application/api/ClienteApi.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
-@RequestMapping("/v1/cliete")
+@RequestMapping("/v1/cliente")
 public interface ClienteApi {
 	
 	@PostMapping
