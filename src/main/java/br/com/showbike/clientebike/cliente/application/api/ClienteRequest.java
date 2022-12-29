3e48819c8d854890461a2941aa7c0252864ce945
Marcio Moura <marcioavb.ms@gmail.com>
2022-12-29 10:40:10 -0300

    implementa request e response

--- src/main/java/br/com/showbike/clientebike/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/showbike/clientebike/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,27 @@
 package br.com.showbike.clientebike.cliente.application.api;
 
-public class ClienteRequest {
+import java.time.LocalDate;
 
+import org.hibernate.validator.constraints.br.CPF;
+
+import br.com.showbike.clientebike.cliente.domain.Sexo;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
+public class ClienteRequest {
+	private String nomeCompleto;
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	private String celular;
+	private String telefone;
+	private Sexo sexo;
+	@NotNull
+	private LocalDate dataNascimento;
+	@CPF
+	private String cpf;
 }
--- src/main/java/br/com/showbike/clientebike/cliente/application/api/ClienteResponse.java
+++ src/main/java/br/com/showbike/clientebike/cliente/application/api/ClienteResponse.java
@@ -1,5 +1,10 @@
 package br.com.showbike.clientebike.cliente.application.api;
 
-public class ClienteResponse {
+import java.util.UUID;
 
+import lombok.Value;
+
+@Value
+public class ClienteResponse {
+	private UUID idCliente;
 }
