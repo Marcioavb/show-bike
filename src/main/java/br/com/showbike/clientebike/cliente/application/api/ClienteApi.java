c0460816dcf2c887a63282e7edb410ea6764b18b
Marcio Moura <marcioavb.ms@gmail.com>
2022-12-30 09:01:14 -0300

    implementas fluxo api

--- src/main/java/br/com/showbike/clientebike/cliente/application/api/ClienteApi.java
+++ src/main/java/br/com/showbike/clientebike/cliente/application/api/ClienteApi.java
@@ -1,11 +1,16 @@
 package br.com.showbike.clientebike.cliente.application.api;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/v1/cliente")
@@ -13,5 +18,9 @@ public interface ClienteApi {
 	
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
-	ClienteResponse postCliente (@RequestBody ClienteRequest clienteRequest);
+	ClienteResponse postCliente (@Valid @RequestBody ClienteRequest clienteRequest);
+	
+	@GetMapping
+	@ResponseStatus(code = HttpStatus.OK)
+	List<ClienteListResponse> getTodosClientes();
 }
---
+++ src/main/java/br/com/showbike/clientebike/cliente/application/api/ClienteListResponse.java
@@ -0,0 +1,14 @@
+package br.com.showbike.clientebike.cliente.application.api;
+
+import java.util.UUID;
+
+import lombok.Value;
+
+@Value
+public class ClienteListResponse {
+	private UUID idCliente;
+	private String nomeCompleto;
+	private String cpf;
+	private String email;
+	private String celular;
+}
